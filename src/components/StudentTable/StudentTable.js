1908ea1192f5a39bfc57b75a399d08b8fbfa1ef3
BrianPuente <brianpuente.23@gmail.com>
2025-01-31 02:02:50 -0300

    Tabla para listar los alumnos funcionando

--- src/components/StudentTable/StudentTable.js
+++ src/components/StudentTable/StudentTable.js
@@ -1,9 +1,57 @@
-// src/components/StudentsTable/StudentsTable.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaFileAlt } from 'react-icons/fa';
 import './StudentTable.css';
 
 const StudentsTable = () => {
+  const [students, setStudents] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        console.log('Token:', token); // Depuración: verifica el token
+
+        const url = `${process.env.REACT_APP_API_URL}/api/estudiantes/listado`;
+        console.log('URL:', url); // Depuración: verifica la URL
+
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        console.log('Response status:', response.status); // Depuración: verifica el estado de la respuesta
+        const responseText = await response.text();
+        console.log('Response text:', responseText); // Depuración: verifica el texto de la respuesta
+
+        if (!response.ok) {
+          throw new Error('Error al cargar el listado de alumnos');
+        }
+
+        const data = JSON.parse(responseText);
+        setStudents(data);
+      } catch (err) {
+        console.error('Error al cargar la lista:', err.message);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchList();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div className="error-message">Error: {error}</div>;
+  }
+
   return (
     <div className="students-table-container">
       <table className="students-table">
@@ -17,12 +65,12 @@ const StudentsTable = () => {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 20 }).map((_, index) => (
+          {students.map((student, index) => (
             <tr key={index}>
-              <td>Rosa Altamirano</td>
-              <td>rosa@gmail.com</td>
-              <td>320</td>
-              <td>5</td>
+              <td>{student.nombre}</td>
+              <td>{student.email}</td>
+              <td>{student.sesiones}</td>
+              <td>{student.pacientes}</td>
               <td>
                 <FaFileAlt className="icon" />
               </td>
